aeec764595824791cb7b2afef7f81031d9f64b06
Afonso Mateus <afonsomateus@zeke.local>
2021-04-07 22:28:05 -0300

    Projeto finalizado

---
+++ src/components/Profile.tsx
@@ -0,0 +1,19 @@
+import { useContext } from 'react';
+import { ChallengeContext } from '../contexts/ChallengeContext';
+import styles from '../styles/components/Profile.module.css';
+
+export function Profile() {
+    const { level } = useContext(ChallengeContext);
+    return (
+        <div className={styles.profileContainer}>
+            <img src="https://github.com/afonsomateus21.png" alt="Afonso Mateus"/>
+            <div>
+                <strong>Afonso Mateus</strong>
+                <p>
+                    <img src="icons/level.svg" alt="Level"/>
+                    Level {level}
+                </p>
+            </div>
+        </div>
+    );
+}
